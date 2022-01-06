48260232740ae65bc9d414d94bd3c02c8d7c8662
Tiffany MANOLIS <tiffany.mano@gmail.com>
2022-01-06 15:19:06 +0100

    test

--- srcs/main.c
+++ srcs/main.c
@@ -1,34 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosophers.h                                     :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tmanolis <tmanolis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/06 15:14:29 by tmanolis          #+#    #+#             */
-/*   Updated: 2022/01/06 15:14:35 by tmanolis         ###   ########.fr       */
+/*   Created: 2022/01/06 15:14:39 by tmanolis          #+#    #+#             */
+/*   Updated: 2022/01/06 15:17:39 by tmanolis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILOSOPHERS_H
-# define PHILOSOPHERS_H
+#include "philosophers.h"
 
-# include "../Libft/libft.h"
-# include <pthread.h>
-
-# define SUCCESS 1
-# define FAILURE 0
-
-# define MAUVE "\033[38;5;98m"
-# define RESET "\033[0m"
-
-
-typedef struct s_data
+int	main(int argc, char **argv)
 {
 	
-}				t_data;
-
-// EVENTS --- check_before_moving
-
-
-#endif
+}
